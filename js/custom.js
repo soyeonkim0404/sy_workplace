db075040c40440f6f851df3548b0fa63cf163cc8
soyeonKim9104 <soyean7979@gmail.com>
2019-12-05 17:04:46 +0900

    2019-12-05

--- js/custom.js
+++ js/custom.js
@@ -6,7 +6,28 @@ const navIcon = document.querySelector('.navIcon');
 const cate = document.getElementById('cate');
 
 navIcon.onclick = () => {
+    //console.log('ddd');
     navIcon.classList.toggle('active');
     cate.classList.toggle('active');
 };
 
+/*progressbar - scroll custom*/
+/*window.addEventListener('scroll', function (e) {
+    //alert('ddd');
+    const scroll = document.body.scrollTop,
+        dh = document.body.offsetHeight,
+        wh = document.body.clientHeight;
+    scrollPercent = (scroll / (dh-wh)) * 100;
+    console.log(e);
+    //$('#progressbar').css('height', scrollPercent + '%');
+});*/
+
+$(window).scroll(function () {
+    var scroll = $(window).scrollTop(),
+        dh = $(document).height(),
+        wh = $(window).height();
+    scrollPercent = (scroll / (dh-wh)) * 100;
+    $('#progressbar').css('height', scrollPercent + '%');
+    console.log(dh, wh);
+
+});
